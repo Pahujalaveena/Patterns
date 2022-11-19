d0444c6150142b8be0d90fbe3e72a0bb7b251a75
Pahujalaveena <113932098+Pahujalaveena@users.noreply.github.com>
2022-11-19 18:25:04 +0000

    Add files via upload

---
+++ Pattern4.cpp
@@ -0,0 +1,32 @@
+/*
+    *
+   **
+  ***
+ ****
+*****
+*/
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number!"<<endl;
+    cin>>n;
+    	for(int i=0;i<=n;i++)
+	{
+		for(int j=0;j<=n;j++)
+		{
+		   if(j<=n-i)
+		   {
+		   	cout<<" ";
+		   }
+		   else
+		   cout<<"*";
+		}
+		cout<<endl;
+	}
+ 	
+	return 0;
+}
