72a66c29cf8c42f0e225a36c3f0ead3510ee3a9a
Pahujalaveena <113932098+Pahujalaveena@users.noreply.github.com>
2022-12-29 18:22:45 +0000

    Add files via upload

---
+++ pattern13.cpp
@@ -0,0 +1,39 @@
+/*
+
+
+******
+*    *
+*    *
+*    *
+*    *
+******
+
+
+
+*/
+
+
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int n=6;
+	
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=n;j++)
+		{
+			if(j==1 || j==n || i==1 || i==n)
+			{
+				cout<<"*";
+			}
+			else
+			{
+				cout<<" ";
+			}
+		}
+		cout<<endl;
+	}
+}
